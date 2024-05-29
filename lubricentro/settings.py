a70cc075d3ce05a7cc8df7012d6930ccb73b6650
MarianoMonzi <arielmariano.monzon2002@gmail.com>
2024-05-28 23:32:20 -0300

    database

--- lubricentro/settings.py
+++ lubricentro/settings.py
@@ -5,6 +5,8 @@ import dj_database_url
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -73,10 +75,7 @@ WSGI_APPLICATION = 'lubricentro.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
