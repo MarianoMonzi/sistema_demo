80872c5cad6168db5c807017d20de2cb10c5f51a
MarianoMonzi <arielmariano.monzon2002@gmail.com>
2024-05-28 23:19:46 -0300

    demo fixed

--- lubricentro/settings.py
+++ lubricentro/settings.py
@@ -122,3 +122,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'pageApp/static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
